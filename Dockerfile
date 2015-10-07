0329f759b701d84bad8f2062d692af1d9d5c4b47
Lucas Bickel <hairmare@purplehaze.ch>
2015-10-07 19:53:09 +0200

    Build a complete yum repo

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM centos
 
-RUN yum install -y rpmdevtools
+RUN yum install -y rpmdevtools createrepo
 
 COPY create_rpm.sh /usr/local/bin/create_rpm.sh
 COPY triumph-adler.spec /usr/local/share/triumph-adler.spec
--- create_rpm.sh
+++ create_rpm.sh
@@ -1,4 +1,8 @@
 mkdir -p /root/rpmbuild/SOURCES
 spectool -g /usr/local/share/triumph-adler.spec -C /root/rpmbuild/SOURCES/
 rpmbuild -bb /usr/local/share/triumph-adler.spec
-find /root/rpmbuild/RPMS/ -exec cp {} /rpmbuild/ \;
+mkdir -p /tmp/rpmbuild
+find /root/rpmbuild/RPMS/ -exec cp {} /tmp/rpmbuild/ \;
+cd /tmp/rpmbuild
+createrepo .
+cp -r /tmp/rpmbuild/* /rpmbuild/
